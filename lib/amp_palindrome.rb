e148d2a97fd33951562ee30eaa3b308de48bd659
Adam Parker <amichaelparker@gmail.com>
2020-01-27 09:26:27 -0600

    tests added

--- lib/amp_palindrome.rb
+++ lib/amp_palindrome.rb
@@ -1,6 +1,20 @@
 require "amp_palindrome/version"
 
-module AmpPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  def letters
+    self.gsub(/\W/, '')
+  end
+
+  private
+
+  # Returns content for palindrome testing.
+  def processed_content
+    self.letters.downcase
+  end
 end
